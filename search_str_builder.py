15df1dac75e0c1c49f38a6ad5cbfb14a68e8ea96
Cristiano Castro <crisgc1@gmail.com>
2015-03-18 14:13:56 -0300

    Criando a função de busca de string de pesquisa em python.
    O script de criação da string de busca será transformada para python

---
+++ search_str_builder.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Possui funções para montar strings de pesquisa Para uma determinada plataforma.
+
+Serão tranformadas em string de pesquisa estruturas de dados do tipo JSON:
+    Exemplo:
+    {
+        groups: {
+            group1: [term1, term2, term3]
+        }
+        1: {
+            __and__: {
+                group: group1
+            }
+                
+        }
+    }
+
+Os operadores lógicos permitidos na montagem da linha serão __and__ e __or__.
+Esses operadores serão transformados
+"""
+
+# A biblioterca JSON pode ser lida utilizando a biblioteca padrão JSON
+# (goo.gl/32pvde)
+
+# O tipo de um objeto pode ser testado através da função type:  (goo.gl/8Mh2W0)
+
+
+# TODO verificar como pode ser lido argumentos da linha de
+
+
