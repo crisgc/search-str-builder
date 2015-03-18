ce8f2b20f82ee682590fef9d37912d885a8a1356
Cristiano Gurgel de Castro <crisgc1@gmail.com>
2015-03-18 19:55:47 -0300

    Continuando a tradução do script para montar a string de pesquisa.
    Separando os métodos para montar as strings.

--- search_str_builder.py
+++ search_str_builder.py
@@ -25,7 +25,7 @@ Esses operadores serão transformados
 # A biblioterca JSON pode ser lida utilizando a biblioteca padrão JSON
 # (goo.gl/32pvde)
 
-# O tipo de um objeto pode ser testado através da função type:  (goo.gl/8Mh2W0)
+# O tipo de um objeto pode ser testado através da função type:  ("K!=goo.gl/8Mh2W0)
 
 
 # TODO verificar como pode ser lido argumentos da linha de
@@ -35,14 +35,49 @@ Programa para gerar uma string de pesquisa a partir de grupos e conectores
 logicos
 """
 
-def build_search_string(syntax_tree, map_function):
+DEFAULT_MAP = dict(__and__='and', __or__='or')
+
+def is_complex_string(string_to_test):
     """
-    Monta
+    Testa se uma string é complexa. Isto é se contém um espaço
     """
-    pass    
+    return (' ' in string_to_test)
+
+
+def build_search_str(string_ast, groups, map_dict):
+    """
+    Monta um string de pesquisa
+    """
+    if isinstance(string_ast, basicstring):
+        if
+        return string_ast
+
+def build_search_string(search_dict, map_dict=None):
+    # TODO essa função deve separar as strings e os grupos
+    """
+    Monta as strings de pesquisa
+
+    Args:
+        syntax_tree: a string de pesquisa
+    """
+    if map_dict is None:
+        map_dict = DEFAULT_MAP
+
+    string_trees = [ value for (key,value) in search_dict.iteritems() if type(key)
+            == int ]
+
+    pass   
 
 def main():
-    pass
+    ast = {'__groups__': 
+            { 
+                'group1': ['test']
+                }, 
+            1 : {'__group__': 'group1'}
+            }
+    build_search_string(ast)
 
 if __name__ == '__main__':
+    # TODO transformar esse teste em um teste unitário
+
     main()
