3e644a19acb20eb5d96b82fb12ed25d8b72b8128
Cristiano Gurgel de Castro <crisgc1@gmail.com>
2015-03-18 19:07:19 -0300

    Començando a transformar o script que monta a string de pesquisa para python
    Passando o script da linguagem OCaml para python.

---
+++ search_str_builder.py
@@ -0,0 +1,15 @@
+# coding=utf8
+
+"""
+Programa para gerar uma string de pesquisa a partir de grupos e conectores
+logicos
+"""
+
+def build_search_string(syntax_tree, map_function):
+    
+
+def main():
+    
+
+if __name__ == '__main__':
+    main()
